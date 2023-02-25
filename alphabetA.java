8a2afa4a08fc88e747260498b7f3ea3bed860f33
Brajesh Kumar <brajeshk922@gmail.com>
2023-02-25 22:45:36 +0530

    alphabet a

---
+++ alphabetA.java
@@ -0,0 +1,12 @@
+public class alphabetA {
+    public static void main(String[] args) {
+        for(int i=1;i<=6;i++){
+            if(i==1 || i==3){
+                System.out.print("***");
+            } else {
+                System.out.print("* *");
+            }
+            System.out.println();
+        }
+    }
+}
